fa7f47b2e058ca4497004459f3df423a43b30f82
Samer Albahra <salbahra@gmail.com>
2014-08-09 08:09:24 -0500

    Base: Add barcode scanning plugin

--- www/js/main.js
+++ www/js/main.js
@@ -191,7 +191,20 @@ $.ajaxSetup({
 
 // Check configuration
 function check_configured(firstLoad) {
-
+    if (firstLoad) {
+        $.mobile.loading("hide");
+        cordova.plugins.barcodeScanner.scan(
+          function (result) {
+              alert("We got a barcode\n" +
+                    "Result: " + result.text + "\n" +
+                    "Format: " + result.format + "\n" +
+                    "Cancelled: " + result.cancelled);
+          },
+          function (error) {
+              alert("Scanning failed: " + error);
+          }
+       );
+    }
 }
 
 // Accessory functions for jQuery Mobile
