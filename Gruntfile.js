6e599db87736b361ed404cfec04eec4a4818395b
Samer Albahra <salbahra@gmail.com>
2014-08-10 23:47:24 -0500

    Base: Support pre versions

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,11 +5,14 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks("grunt-text-replace");
   grunt.loadNpmTasks("grunt-shell");
 
+  // Get option `pre` on bump command
+  var isPre = grunt.option("pre") ? true : false;
+
   var bumpVersion = function(version,level) {
     version = version.split(".") || [0,0,0];
     level = level || 2;
     version[level]++;
-    return version.join(".");
+    return version.join(".")+(isPre ? "-pre" : "");
   };
 
   // Project configuration.
@@ -41,9 +44,13 @@ module.exports = function(grunt) {
         src: ["www/index.html"],
         overwrite: true,
         replacements: [{
-          from: /<p>Version: ([\d|\.]+)<\/p>/g,
+          from: /<p>Version: ([\d|\.]+)(-pre)?<\/p>/g,
           to: function(matchedWord, index, fullText, regexMatches){
-            return "<p>Version: "+bumpVersion(regexMatches[0])+"</p>";
+            if (regexMatches[1]) {
+              return "<p>Version: "+regexMatches[0]+"</p>";
+            } else {
+              return "<p>Version: "+bumpVersion(regexMatches[0])+"</p>";
+            }
           }
         }]
       },
@@ -51,14 +58,20 @@ module.exports = function(grunt) {
         src: ["www/config.xml"],
         overwrite: true,
         replacements: [{
-          from: /version     = "([\d|\.]+)"/g,
+          from: /version     = "([\d|\.]+)(-pre)?"/g,
           to: function(matchedWord, index, fullText, regexMatches){
-            return "version     = \""+bumpVersion(regexMatches[0])+"\"";
+            if (regexMatches[1]) {
+              return "version     = \""+regexMatches[1]+"\"";
+            } else {
+              return "version     = \""+bumpVersion(regexMatches[0])+"\"";
+            }
           }
         },{
           from: /versionCode = "(\d+)"/g,
           to: function(matchedWord, index, fullText, regexMatches) {
-            return "versionCode = \""+(parseInt(regexMatches[0])+1)+"\"";
+            if (!isPre) {
+              return "versionCode = \""+(parseInt(regexMatches[0])+1)+"\"";
+            }
           }
         }]
       },
@@ -66,9 +79,13 @@ module.exports = function(grunt) {
         src: ["manifest.json","manifest.webapp","package.json"],
         overwrite: true,
         replacements: [{
-          from: /"version": "([\d|\.]+)"/g,
+          from: /"version": "([\d|\.]+)(-pre)?"/g,
           to: function(matchedWord, index, fullText, regexMatches){
-            return "\"version\": \""+bumpVersion(regexMatches[0])+"\"";
+            if (regexMatches[1]) {
+              return "\"version\": \""+regexMatches[0]+"\"";
+            } else {
+              return "\"version\": \""+bumpVersion(regexMatches[0])+"\"";
+            }
           }
         }]
       }
